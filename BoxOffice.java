78d0a8c88792d5619e90cdb999167a52b30ce8a6
saltoftheearth1 <crowley.liam@student.ccm.edu>
2026-09-10 23:09:08 -0400

    Finished BoxOffice.java according to instructions

--- BoxOffice.java
+++ BoxOffice.java
@@ -1,4 +1,43 @@
+import java.util.Scanner;
 public class BoxOffice {
-    
-    
+    public static void main(String[]) {
+
+        //create scanner for user input
+        Scanner keyboard = new Scanner(System.in);
+
+        // ask user for movie info
+        System.out.print("Enter the movie name: ");
+        String movieName = keyboard.nextLine();
+
+        System.out.print("Enter the number of adult tickets sold: ");
+        int adultTickets = keyboard.nextInt();
+
+        System.out.print("Enter the number of child tickets sold: ");
+        int childTickets = keyboard.nextInt();
+
+        // ticket price
+        double adultPrice = 10.00;
+        double childPrice = 6.00;
+
+        // calc gross box office revenue
+        double grossBoxOffice = (adultTickets * adultPrice)
+                + (childTickets * childPrice);
+
+        // theater keeps 20% of the gross revenue
+        double netBoxOffice = grossBoxOffice * 0.20;
+
+        // distributor receives the last 80%
+        double distributorAmount = grossBoxOffice * 0.80;
+
+        // display results, %30-s added to make everything even
+        System.out.printf("%-30s %s%n", "Movie Name:", movieName);
+        System.out.printf("%-30s %d%n", "Adult Tickets Sold:", adultTickets);
+        System.out.printf("%-30s %d%n", "Child Tickets Sold:", childTickets);
+        System.out.printf("%-30s $%.2f%n", "Gross Box Office Profit:", grossBoxOffice);
+        System.out.printf("%-30s $%.2f%n", "Net Box Office Profit:", netBoxOffice);
+        System.out.printf("%-30s $%.2f%n", "Amount Paid to Distributor:", distributorAmount);
+
+        // close scanner
+        keyboard.close();
+    }
 }
